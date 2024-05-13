3828d84271fee94ac42a5e27f0e4e3f95a22254e
Patricia <patricia.alejandra.pacheco.reina@nl.abnamro.com>
2024-05-13 12:50:31 +0200

    Add abstract class to bypass mypy

--- src/model/model.py
+++ src/model/model.py
@@ -1,14 +1,20 @@
-from collections import defaultdict
-import pandas as pd
 import pickle
-import xgboost as xgb
-
+from abc import ABC, abstractmethod
+from collections import defaultdict
 from typing import Dict
-from sklearn.preprocessing import MinMaxScaler
+
+import pandas as pd
+import xgboost as xgb
+from sklearn.metrics import (
+    mean_absolute_error,
+    r2_score,
+    root_mean_squared_error,
+)
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import mean_absolute_error, root_mean_squared_error, r2_score
+from sklearn.preprocessing import MinMaxScaler
 
 
+# adding comment to trigger pre-commit#
 class ModelIsNone(Exception):
     """
     Raised when trying to predict or evaluate a model that is None
@@ -17,9 +23,53 @@ class ModelIsNone(Exception):
     pass
 
 
-class VolumeForecastModel():
+class Model(ABC):
     """
     Abstract class that defines a common interface for model classes
+
+    Raises:
+        ModelIsNone: Exception raised when trying to fit or predict a
+        model that has not being trained or loaded from MLflow
+    """
+
+    MODEL_NONE_ERROR_MSG = "The model is neither trained nor loaded from mlflow"
+    WRONG_MODEL_PATH = "The provided path does not exists"
+
+    def __init__(
+        self,
+    ) -> None:
+        """
+
+        Args:
+            categorical_features (Union[List[str], None]): Column names of the categorical features
+            numeric_features (Union[List[str], None]): Column names of the numeic features
+        """
+        self._model: Pipeline | None = None
+
+    @property
+    def model(self):
+        return self._model
+
+    @abstractmethod
+    def fit(
+        self,
+        x_train: pd.DataFrame,
+        y_train: pd.DataFrame,
+    ):
+        pass
+
+    @abstractmethod
+    def predict(self, x: pd.DataFrame):
+        pass
+
+    @abstractmethod
+    def evaluate(self, x: pd.DataFrame, y: pd.DataFrame) -> Dict:
+        pass
+
+
+class VolumeForecastModel(Model):
+    """
+    Class for Volume Forecast Model
 
     Raises:
         ModelIsNone: Exception raised when trying to fit or predict a
@@ -34,24 +84,22 @@ class VolumeForecastModel():
         learning_rate: float | None,
         max_depth: int | None,
         subsample: float | None,
-        random_state: int | None
-
+        random_state: int | None,
     ) -> None:
-        """
-
-        Args:
-            categorical_features (Union[List[str], None]): Column names of the categorical features
-            numeric_features (Union[List[str], None]): Column names of the numeic features
-        """
-        self._model: Pipeline | None = None
-        if learning_rate is not None and max_depth is not None and subsample is not None and random_state is not None:
-            self._model = self._get_pipeline(learning_rate, max_depth, subsample, random_state)
-
+        super().__init__()
+        if (
+            learning_rate is not None
+            and max_depth is not None
+            and subsample is not None
+            and random_state is not None
+        ):
+            self._model = self._get_pipeline(
+                learning_rate, max_depth, subsample, random_state
+            )
 
     @property
     def model(self):
         return self._model
-
 
     @model.setter
     def model(self, path: str):
@@ -64,28 +112,18 @@ class VolumeForecastModel():
             OSError: Raised when the path is incorrect
         """
         try:
-            with open(path, 'rb') as file:
+            with open(path, "rb") as file:
                 self._model: Pipeline = pickle.load(file)
         except OSError:
             raise OSError(self.WRONG_MODEL_PATH + f" {path}")
 
-    def fit(
-        self,
-        x_train: pd.DataFrame,
-        y_train: pd.DataFrame,
-    ):
-        if self._model is None:
-            raise ModelIsNone(self.MODEL_NONE_ERROR_MSG)
-        self._model = self._model.fit(x_train, y_train)
-
-
     def _get_pipeline(
-            self,
-            learning_rate: float = 0.2,
-            max_depth: int = 5,
-            subsample: float = 0.8,
-            random_state: int = 42
-        ) -> Pipeline:
+        self,
+        learning_rate: float = 0.2,
+        max_depth: int = 5,
+        subsample: float = 0.8,
+        random_state: int = 42,
+    ) -> Pipeline:
         preprocessor = Pipeline(
             steps=[
                 ("scaler", MinMaxScaler()),
@@ -96,10 +134,18 @@ class VolumeForecastModel():
             learning_rate=learning_rate,
             max_depth=max_depth,
             subsample=subsample,
-            random_state=random_state
+            random_state=random_state,
         )
         return Pipeline(steps=[("preprocessor", preprocessor), ("model", xgb_model)])
 
+    def fit(
+        self,
+        x_train: pd.DataFrame,
+        y_train: pd.DataFrame,
+    ):
+        if self._model is None:
+            raise ModelIsNone(self.MODEL_NONE_ERROR_MSG)
+        self._model = self._model.fit(x_train, y_train)
 
     def predict(self, x: pd.DataFrame):
         """_summary_
@@ -117,7 +163,6 @@ class VolumeForecastModel():
             raise ModelIsNone(self.MODEL_NONE_ERROR_MSG)
         predictions = self._model.predict(x)
         return predictions
-
 
     def evaluate(self, x: pd.DataFrame, y: pd.DataFrame) -> Dict:
         """_summary_
